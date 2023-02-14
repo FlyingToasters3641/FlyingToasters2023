af0ccea31f019acb2cc41f0cdfd992539867b80f
James Partin <jamespartin@Jamess-MacBook-Air.local>
2023-02-14 15:18:13 -0500

    added check to only delete the first treemap entry if the size is above 10

--- src/main/java/frc/robot/subsystems/Vision/AprilTagSubsystem/TimeCorrectedAprilTag.java
+++ src/main/java/frc/robot/subsystems/Vision/AprilTagSubsystem/TimeCorrectedAprilTag.java
@@ -14,31 +14,32 @@ import edu.wpi.first.wpilibj.TimesliceRobot;
 import frc.robot.subsystems.Vision.VisionHelpers.AprilTagConfig;
 
 /**
-     * This is an extension of the wpilib AprilTag class that adds the
-     * ability to place the active position of the april tag in the class
-     * to pass it around
-     */
-    public class TimeCorrectedAprilTag extends AprilTag{
-        public Pose3d relativePose;
-        double timeStamp;
-        public double ambiguity;
-        NetworkTable table;
-        Double maxHistTime = 1.0;
-        //  Timestamp, <pose, ambiguity>
-        TreeMap<Double, Map<Pose3d, Double>> historicalValues = new TreeMap<Double, Map<Pose3d, Double>>();
+ * This is an extension of the wpilib AprilTag class that adds the
+ * ability to place the active position of the april tag in the class
+ * to pass it around
+ */
+public class TimeCorrectedAprilTag extends AprilTag {
+    public Pose3d relativePose;
+    double timeStamp;
+    public double ambiguity;
+    NetworkTable table;
+    int maxHistValues = 10;
+    // Timestamp, <pose, ambiguity>
+    TreeMap<Double, Map<Pose3d, Double>> historicalValues = new TreeMap<Double, Map<Pose3d, Double>>();
 
-        public TimeCorrectedAprilTag(AprilTagConfig config, NetworkTable table) {
-            super(config.getId(), config.getPose());
-            this.table = table;
-        }
+    public TimeCorrectedAprilTag(AprilTagConfig config, NetworkTable table) {
+        super(config.getId(), config.getPose());
+        this.table = table;
+    }
 
-        public void updatePosition(Pose3d relativePose, double timeStamp, double ambiguity) {
-            Map positions = new HashMap<Pose3d, Double>();
-            positions.put(relativePose, ambiguity);
-            historicalValues.put(timeStamp, positions);
+    public void updatePosition(Pose3d relativePose, double timeStamp, double ambiguity) {
+        Map positions = new HashMap<Pose3d, Double>();
+        positions.put(relativePose, ambiguity);
+        historicalValues.put(timeStamp, positions);
+        if (historicalValues.size() >= maxHistValues) {
             historicalValues.remove(historicalValues.firstKey());
         }
-        
-
 
     }
+
+}
