28e64dcc0d8a436dcf6d586be6112551475a7d6a
James Partin <jamespartin@Jamess-MacBook-Air.local>
2023-02-14 15:10:14 -0500

    fleshed out historical value storage in apriltag

--- src/main/java/frc/robot/subsystems/Vision/AprilTagSubsystem/TimeCorrectedAprilTag.java
+++ src/main/java/frc/robot/subsystems/Vision/AprilTagSubsystem/TimeCorrectedAprilTag.java
@@ -1,8 +1,16 @@
 package frc.robot.subsystems.Vision.AprilTagSubsystem;
 
+import java.security.Timestamp;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.TimesliceRobot;
 import frc.robot.subsystems.Vision.VisionHelpers.AprilTagConfig;
 
 /**
@@ -12,9 +20,12 @@ import frc.robot.subsystems.Vision.VisionHelpers.AprilTagConfig;
      */
     public class TimeCorrectedAprilTag extends AprilTag{
         public Pose3d relativePose;
-        public double timeStamp;
+        double timeStamp;
         public double ambiguity;
         NetworkTable table;
+        Double maxHistTime = 1.0;
+        //  Timestamp, <pose, ambiguity>
+        TreeMap<Double, Map<Pose3d, Double>> historicalValues = new TreeMap<Double, Map<Pose3d, Double>>();
 
         public TimeCorrectedAprilTag(AprilTagConfig config, NetworkTable table) {
             super(config.getId(), config.getPose());
@@ -22,9 +33,12 @@ import frc.robot.subsystems.Vision.VisionHelpers.AprilTagConfig;
         }
 
         public void updatePosition(Pose3d relativePose, double timeStamp, double ambiguity) {
-            this.relativePose = relativePose;
-            this.timeStamp = timeStamp;
-            this.ambiguity = ambiguity;
+            Map positions = new HashMap<Pose3d, Double>();
+            positions.put(relativePose, ambiguity);
+            historicalValues.put(timeStamp, positions);
+            historicalValues.remove(historicalValues.firstKey());
         }
-    
+        
+
+
     }
