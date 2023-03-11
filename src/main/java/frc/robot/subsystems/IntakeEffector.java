924597f8aee365f1b8aea7b8227d0e8c3ba574d0
Lizzy Partin <eclaire0110@gmail.com>
2023-03-11 10:46:48 -0500

    got rid of unused imports

--- src/main/java/frc/robot/subsystems/IntakeEffector.java
+++ src/main/java/frc/robot/subsystems/IntakeEffector.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.util.MotorHelper;
 import frc.robot.Constants.DrivetrainConstants;
-import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
