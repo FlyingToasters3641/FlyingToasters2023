10f08fdb3c266f3cd80fd7c1e1486e0642d8a1b2
James Partin <jamesaustinpartin@gmail.com>
2023-03-17 10:52:20 -0400

    autobalance

--- src/main/java/frc/robot/commands/AutoBalanceAlt.java
+++ src/main/java/frc/robot/commands/AutoBalanceAlt.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivetrainSubsystem;
@@ -11,11 +12,14 @@ public class AutoBalanceAlt extends CommandBase {
     PoseEstimatorSubsystem poseEstimator;
     double pitch = 0.0;
     double correction;
+    double prevAngle;
     double roll = 0.0;
     double angularVelocity;
     double angleDegrees;
     final int POSITION_THRESHOLD = 3; // degrees
-    final int VELOCITY_THRESHOLD = 8; // degrees/sec
+    double startTime = 0;
+    double prevTime = 0;
+    final double VELOCITY_THRESHOLD = 1; // degrees/sec 20
 
     public AutoBalanceAlt(DrivetrainSubsystem drive, PoseEstimatorSubsystem poseEstimator) {
         this.drive = drive;
@@ -24,18 +28,19 @@ public class AutoBalanceAlt extends CommandBase {
 
     @Override
     public void execute() {
+        double startTime = Timer.getFPGATimestamp();
+        double timeDelta = prevTime - startTime;
         var rotation = poseEstimator.getCurrentPose().getRotation();
         pitch = drive.getGyroscopeRotation3d().getY();
         roll = drive.getGyroscopeRotation3d().getX();
         angleDegrees = rotation.getCos() * Math.toDegrees(roll)
                 + rotation.getSin() * Math.toDegrees(pitch);
 
-        double angleVelocityDegreesPerSec = rotation.getCos() * drive.getRollDegreesSec()
-                + rotation.getSin() * drive.getPitchDegreesSec();
+        double angleVelocityDegreesPerSec = (angleDegrees - prevAngle) / timeDelta;
 
         boolean shouldStop = (angleDegrees < 0.0 && angleVelocityDegreesPerSec > VELOCITY_THRESHOLD)
                 || (angleDegrees > 0.0
-                        && angleVelocityDegreesPerSec < -VELOCITY_THRESHOLD);
+                && angleVelocityDegreesPerSec < -VELOCITY_THRESHOLD);
 
         correction = angleDegrees > 0 ? -0.4 : 0.4;
 
@@ -46,13 +51,16 @@ public class AutoBalanceAlt extends CommandBase {
         SmartDashboard.putNumber("Balancing: Angular Velocity (Degrees/sec)", angleVelocityDegreesPerSec);
         SmartDashboard.putBoolean("Balancing: Shoud stop?", shouldStop);
         SmartDashboard.putNumber("Balancing: Correction (m/sec)", correction);
+        prevAngle = angleDegrees;
+        prevTime = startTime;
+        if (!shouldStop) {
+            drive.drive(new ChassisSpeeds(0, 0, 0));
 
-        if (shouldStop) {
             drive.stop();
         } else {
             drive.drive(
                     ChassisSpeeds.fromFieldRelativeSpeeds(
-                            correction,
+                            - correction,
                             0.0,
                             0.0,
                             rotation));
