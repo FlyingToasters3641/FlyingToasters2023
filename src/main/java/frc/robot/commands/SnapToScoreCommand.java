0e4350805a01967992cf9aea74d17f2773112d11
James Partin <jamesaustinpartin@gmail.com>
2023-03-30 15:47:42 -0400

    changed behavior of snaptoscore

--- src/main/java/frc/robot/commands/SnapToScoreCommand.java
+++ src/main/java/frc/robot/commands/SnapToScoreCommand.java
@@ -8,6 +8,7 @@ import frc.robot.subsystems.PoseEstimatorSubsystem;
 
 public class SnapToScoreCommand extends DriveToPose {
     int bestPosition;
+    Boolean goToClosest = true;
     int oneOver = 0;
     Pose2d[] Positions = new Pose2d[]{
             new Pose2d(1.81301, 5.098288, new Rotation2d().fromDegrees(0)),
@@ -20,27 +21,35 @@ public class SnapToScoreCommand extends DriveToPose {
             new Pose2d(1.81301, 1.08437, new Rotation2d().fromDegrees(0)),
             new Pose2d(1.81301, 0.47977869, new Rotation2d().fromDegrees(0))
     };
-    public SnapToScoreCommand(DrivetrainSubsystem drive, PoseEstimatorSubsystem poseEstimator, int oneOver) {
+    public SnapToScoreCommand(DrivetrainSubsystem drive, PoseEstimatorSubsystem poseEstimator, Boolean goToClosest) {
         super(drive, poseEstimator, (Pose2d) null);
         poseSupplier = this::getBestPosition;
-        this.oneOver = oneOver;
+        this.goToClosest = goToClosest;
     }
 
     @Override
     public void initialize() {
-        var curPose = poseEstimator.getCurrentPose() != null ? poseEstimator.getCurrentPose() : new Pose2d(0,0, new Rotation2d(0));
-        Double prevLength = null;
+        if (goToClosest) {
+            var curPose = poseEstimator.getCurrentPose() != null ? poseEstimator.getCurrentPose() : new Pose2d(0, 0, new Rotation2d(0));
 
-        for (int i = 0; i < Positions.length; i++) {
-            var pose = Positions[i];
-            var poseCompare = Math.abs(pose.getTranslation().getDistance(curPose.getTranslation()));
-            if (prevLength == null || poseCompare < prevLength) {
-                prevLength = Math.abs(pose.getTranslation().getDistance(curPose.getTranslation()));
-                bestPosition = i;
+            Double prevLength = null;
+
+            for (int i = 0; i < Positions.length; i++) {
+                var pose = Positions[i];
+                var poseCompare = Math.abs(pose.getTranslation().getDistance(curPose.getTranslation()));
+                if (prevLength == null || poseCompare < prevLength) {
+                    prevLength = Math.abs(pose.getTranslation().getDistance(curPose.getTranslation()));
+                    bestPosition = i + 1;
+                }
             }
+            SmartDashboard.putNumber("Current Y position:", curPose.getY());
+        } else {
+            bestPosition = (int) SmartDashboard.getNumber("Lining up to score at", 1);
+
         }
-        SmartDashboard.putNumber("Current Y position:", curPose.getY());
-        SmartDashboard.putNumber("Lining up to score at", bestPosition + 1);
+
+        bestPosition -= 1;
+        SmartDashboard.putNumber("Lining up to score at", bestPosition);
     }
 
 
